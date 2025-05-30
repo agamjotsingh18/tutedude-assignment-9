3ec9f3a89180101e9b55ffa3b208baf5f0f0fd2f
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2025-05-30 22:47:15 +0530

    x

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
-mongoose.connect('mongodb+srv://agamjot_singh:FTOevKJIIh7vuGe0@cluster0.trhpj0r.mongodb.net/secretsApp?retryWrites=true&w=majority');
-
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
